bf28fbada6f77a3a71d5f43886e14b49849f8e67
Vaibhav Dhunde <vaibhavdhunde94@gmail.com>
2019-10-03 15:54:18 +0530

    Register user

--- include/DbOperations.php
+++ include/DbOperations.php
@@ -10,7 +10,45 @@ class DbOperations {
         $this->conn = $db->connect();
     }
     
-    /* ------------- implement your database operation functions here ------------- */
+    /* ------------- USERS TABLE OEPRATIONS ------------- */
+
+    function registerUser($name, $email, $password) {
+        // encrypt the password
+        $password_hash = $this->getEncryptedPassword($password);
+
+        // check for email conflict
+        if ($this->isEmailRegistered($email)) {
+            return USER_ALREADY_EXISTS;
+        }
+
+        // add user
+        $stmt = $this->conn->prepare("INSERT INTO `users`(`name`, `email`, `password_hash`) VALUES(?, ?, ?)");
+        $stmt->bind_param("sss", $name, $email, $password_hash);
+        if ($stmt->execute()) {
+            return USER_CREATED_SUCCESSFULLY;
+        } else {
+            return FAILED_TO_CREATE_USER;
+        }
+    }
+
+    /**
+     * Checks whether the given email id already exists in the users table or not.
+     * @param String $email - User's email id
+     */
+    private function isEmailRegistered($email) {
+        $stmt = $this->conn->prepare("SELECT `id` FROM `users` WHERE `email` = ?");
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $stmt->store_result();
+        $num_rows = $stmt->num_rows;
+        return $num_rows > 0;
+    }
+
+    private function getEncryptedPassword($password) {
+        return password_hash($password, PASSWORD_DEFAULT);
+    }
+
+    /* ------------- END USERS TABLE OEPRATIONS ------------- */
 
 }
 
--- v1/index.php
+++ v1/index.php
@@ -17,7 +17,51 @@ $app->post('/hello/{name}', function($request, $response, $args) {
     return $response->write("Hello, " . $args["name"]);
 });
 
-/* ------------ implement your api endpoints here -------------- */
+/* ------------------- USERS TABLE API -------------------------- */
+
+/**
+ * Register new user
+ * 
+ * endpoint - /register
+ * method - POST
+ * params - name, email, password
+ */
+$app->post('/register', function($request, $response, $args) {
+    // check required params
+    if (!hasRequiredParams(array('name', 'email', 'password'), $response)) {
+        return $response;
+    }
+
+    // reading post params
+    $request_data = $request->getParams();
+    $name = $request_data['name'];
+    $email = $request_data['email'];
+    $password = $request_data['password'];
+
+    // check for invalid email address
+    if (!isValidEmail($email, $response)) {
+        return $response;
+    }
+
+    // register user
+    $db = new DbOperations();
+    $result = $db->registerUser($name, $email, $password);
+
+    if ($result == USER_CREATED_SUCCESSFULLY) {
+        $message['error'] = false;
+        $message['message'] = "User registered successfully";
+    } else if ($result == FAILED_TO_CREATE_USER) {
+        $message['error'] = true;
+        $message['message'] = "Problem registering user. Please try again later";
+    } else {
+        $message['error'] = true;
+        $message['message'] = "User with this email address already exists. Please try again";
+    }
+
+    return buildResponse(200, $message, $response);
+});
+
+/* ------------------- END USERS TABLE API -------------------------- */
 
 /* -------------------- HELPER FUNCTIONS ---------------------------- */
 function hasRequiredParams($required_params, $response) {
@@ -47,6 +91,22 @@ function buildResponse($status_code, $message, $response) {
     $response->withHeader('Content-type', 'application/json');
     $response->withStatus($status_code);
     $response->write(json_encode($message));
+}
+
+/**
+ * Validating email address
+ * @param String $email User email address
+ * @return boolean
+ */
+function isValidEmail($email, $response) {
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $message["error"] = true;
+        $message["message"] = "Email address is not valid";
+        buildResponse(400, $message, $response);
+        return false;
+    } else {
+        return true;
+    }
 }
 /* -------------------- END HELPER FUNCTIONS ---------------------------- */
 
